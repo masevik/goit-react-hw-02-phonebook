21d91d3464bcdf0150c6dafe5a72c0e94a61d2f1
Serhii Malychkovych <masevik@gmail.com>
2022-12-28 00:22:54 +0200

    fix loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,6 @@ export class App extends Component {
   };
 
   deleteContact = contactId => {
-    console.log(123);
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
